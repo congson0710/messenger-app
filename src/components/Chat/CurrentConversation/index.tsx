b02679bb72ae186514cca1798f51e0f35e4599af
Tan Bui <tan.bui@employmenthero.com>
2022-05-03 13:08:00 +0700

    Style chat header

--- src/components/Chat/CurrentConversation/index.tsx
+++ src/components/Chat/CurrentConversation/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import { red } from '@mui/material/colors';
 import { ConversationRowType, UserType } from "../../type";
+import ListNameWithAvatar from "../../ListNameWithAvatar";
 
 const CurrentConversation = ({
   currentUser,
@@ -12,7 +17,19 @@ const CurrentConversation = ({
     return <div>Please select a conversation</div>
   }
 
-  return <div>{conversation.participants.map(participant => participant.name).join(', ')}</div>;
+  const recipientName = conversation.participants[0].name;
+
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <ListNameWithAvatar name={recipientName} />
+        }
+        title={recipientName}
+      />
+    </Card>
+  )
+  // return <div>{conversation.participants.map(participant => participant.name).join(', ')}</div>;
 };
 
 export default CurrentConversation;
